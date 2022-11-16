04fd096fba75425a1ceb3b0fe3f67c499a83852f
SerhiiIsaev <97984230+SerhiiIsaev@users.noreply.github.com>
2022-11-16 20:50:25 +0200

    Update Filter.jsx

--- src/components/Phonebook/Filter/Filter.jsx
+++ src/components/Phonebook/Filter/Filter.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import { nanoid } from 'nanoid';
-import css from '../Filter/Filter.module.css';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { setFilter } from '../../../redux/filterSlice';
+import styles from '../Filter/Filter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { setFilter } from '../../../redux/filter/filterSlice';
 
 export const Filter = () => {
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-  // const filter = useSelector(state => state.filter);
+  const filter = useSelector(state => state.filter);
   
-  // const handleChange = (e) => {
-  //   dispatch(setFilter(e.target.value));
-  // }
+  const handleChange = (e) => {
+    dispatch(setFilter(e.target.value));
+  }
   
   const filterId = nanoid();
 
-  return (<div className={css.filterWrapper}>
-    <label htmlFor={filterId} className={css.label}>Find contacts by name</label>
+  return (<div className={styles.filterWrapper}>
+    <label htmlFor={filterId} className={styles.label}>Find contacts by name</label>
     <input
       id={filterId}
       type="text"
       name="filter"
-      // value={filter}
-      // onChange={handleChange}
-      className={css.input}
+      value={filter}
+      onChange={handleChange}
+      className={styles.input}
       placeholder='Input name' />
   </div>);
 };
